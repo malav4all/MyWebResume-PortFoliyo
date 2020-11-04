ca23914060295ef5563065f18806bf0a33da4895
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2020-11-04 12:58:04 +0530

    Add files via upload

---
+++ assests/javascript/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $(this).toggleClass("active");
+        $(".menu").toggleClass("active");
+        // console.log("Jquery Loaded");
+    }) 
+})
